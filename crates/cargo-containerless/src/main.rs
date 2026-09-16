ab3faae7699ce9e28130ce989effa638ff59001b
Henry <mail@henrygressmann.de>
2026-09-16 12:47:47 +0200

    chore: start on cargo-containerless
    Signed-off-by: Henry <mail@henrygressmann.de>

--- crates/cargo-containerless/src/main.rs
+++ crates/cargo-containerless/src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+use clap::{Args, Parser};
+
+#[derive(Debug, Parser)]
+#[command(name = "cargo", bin_name = "cargo")]
+#[command(styles = clap_cargo::style::CLAP_STYLING)]
+enum Cargo {
+    /// Build Rust artifacts and package them as OCI images.
+    #[command(version)]
+    Containerless(Options),
+}
+
+#[derive(Debug, Args)]
+struct Options {
+    #[command(flatten)]
+    manifest: clap_cargo::Manifest,
+
+    #[command(flatten)]
+    workspace: clap_cargo::Workspace,
+
+    #[command(flatten)]
+    features: clap_cargo::Features,
+}
+
+fn main() {
+    match Cargo::parse() {
+        Cargo::Containerless(options) => {
+            let _ = (options.manifest, options.workspace, options.features);
+        }
+    }
+}
